d7a29dded7752d413ef3ce7c6d61f3f8a99316c5
Musbau Adesola <musbauinfo@gmail.com>
2025-03-11 13:30:41 -0500

    added files

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,10 @@
-output "intent_id" {
-  description = "ID of the created intent"
-  value       = aws_lexv2models_intent.intent.id
-}
+# output "intent_id" {
+#   description = "ID of the created intent"
+#   value       = aws_lexv2models_intent.intent.id
+# }
 
-output "bot_id" {
-  description = "ID of the created bot"
-  value       = aws_lexv2models_bot.bot.id
-}
+# output "bot_id" {
+#   description = "ID of the created bot"
+#   value       = aws_lexv2models_bot.bot.id
+# }
 
